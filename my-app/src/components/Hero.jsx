7d1ed9e6312271f2f490ffc94d8aeadf98c4542c
VirakbothSoth <121625231+VirakbothSoth@users.noreply.github.com>
2024-12-17 22:50:14 +0700

    Only added my profile on the website

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import face from '../assets/images/virakboth.png';
 
 const Hero = () => {
   return (
     <section>
       <div className="text-black text-center">
         <div className="m-[150px]">
+          <div className="flex items-center justify-center">
+            <img
+            src={face}
+            alt="Virakboth's Face" 
+            className="h-[500px] rounded-full border-4 border-gray-200 shadow-lg object-cover"
+            />
+          </div>
           <h1 className="inline-block text-9xl font-bold">
             Hello, I'm
           </h1>
