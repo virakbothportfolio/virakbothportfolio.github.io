09eef7d2fcaa6a5c7758739efd727e84fa06be37
VirakbothSoth <121625231+VirakbothSoth@users.noreply.github.com>
2024-12-18 22:49:43 +0700

    Added Animation to Hero

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -6,19 +6,21 @@ const Hero = () => {
     <section>
       <div className="text-black text-center">
         <div className="m-[150px]">
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center animate-bounce">
             <img
             src={face}
             alt="Virakboth's Face" 
             className="h-[500px] rounded-full border-4 border-gray-200 shadow-lg object-cover"
             />
           </div>
-          <h1 className="inline-block text-9xl font-bold">
-            Hello, I'm
-          </h1>
-          <h1 className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-600 text-9xl font-bold m-[20px]">
-            Virakboth
-          </h1>
+          <div className="">
+            <h1 className="inline-block text-9xl font-bold">
+              Hello, I'm
+            </h1>
+            <h1 className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-600 text-9xl font-bold m-[20px]">
+              Virakboth
+            </h1>
+          </div>
           <p className="text-2xl">
             A passionate cambodian Front-End Developer that studied web development since 2023
           </p>
