6642a9604ce8edc8fcf993430b475c84df179f69
VirakbothSoth <121625231+VirakbothSoth@users.noreply.github.com>
2024-12-22 16:32:55 +0700

    Certificates Page (BETA)

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import About from './components/About';
 import Journey from './components/Journey';
@@ -7,20 +8,30 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Certs from './components/Certs';
 import Contact from './components/Contact';
+import CSCerts from './components/CSCerts';
 import './index.css';
-import './styles/tailwind.css'
+import './styles/tailwind.css';
 
 export default function App() {
   return (
-    <div className="text-gray-800 bg-bg">
-      <Hero />
-      <About />
-      <Journey />
-      <Schools />
-      <Projects />
-      <Skills />
-      <Certs />
-      <Contact />
-    </div>
+    <Router>
+      <div className="text-gray-800 bg-bg">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <About />
+              <Journey />
+              <Schools />
+              <Projects />
+              <Skills />
+              <Certs />
+              <Contact />
+            </>
+          } />
+          <Route path="/cscerts" element={<CSCerts />} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
+}
--- my-app/src/components/Certs.jsx
+++ my-app/src/components/Certs.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
-import csx from '../assets/certs/cs50.png';
-import css from '../assets/certs/cs50s.png';
-import csp from '../assets/certs/cs50p.png';
-import Certificate from './Certificate';
+import { Routes, Route, Link } from 'react-router-dom';
+import CSCerts from './CSCerts';
 
 const Certs = () => {
   return (
+    <>
     <section className="p-80">
       <div className="container mx-auto">
         <h2 className="font-bold mb-6">Certificates</h2>
         <div className="py-10 grid grid-cols-1 md:grid-cols-3 gap-10">
-            <Certificate
-              image={csx}
-              url="https://cs50.harvard.edu/certificates/9d966695-a4b6-4154-9952-52e4d2efa692"
-              altText="CS50S Certificate Thumbnail"
-            />
-            <Certificate
-              image={css}
-              url="https://cs50.harvard.edu/certificates/a06a26e2-64e9-438a-8666-977af8d062ed"
-              altText="CS50S Certificate Thumbnail"
-            />
-            <Certificate
-              image={csp}
-              url="https://cs50.harvard.edu/certificates/7226859c-c657-4563-b9c8-99f4873114f6"
-              altText="CS50S Certificate Thumbnail"
-            />
+          <Link to="/cscerts">CS50</Link>
         </div>
       </div>
     </section>
+    <Routes>
+      <Route path="/cscerts" element={<CSCerts />}/>
+    </Routes>
+    </>
   );
 };
 
