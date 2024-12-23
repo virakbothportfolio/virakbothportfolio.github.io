2c085a08f56ae649089a11a85973a71baf8111ad
VirakbothSoth <121625231+VirakbothSoth@users.noreply.github.com>
2024-12-23 23:10:56 +0700

    Added More Certificates

--- my-app/src/assets/certs/index.js
+++ my-app/src/assets/certs/index.js
@@ -2,11 +2,18 @@
 export { default as csx } from './cs50.png';
 export { default as css } from './cs50s.png';
 export { default as csp } from './cs50p.png';
-export { default as m1 } from './m1.png';
-export { default as m2 } from './m2.png';
-export { default as m3 } from './m3.png';
-export { default as m4 } from './m4.png';
-export { default as m5 } from './m5.png';
-export { default as m6 } from './m6.png';
-export { default as m7 } from './m7.png';
-export { default as m8 } from './m8.png';
+
+export { default as jhu } from './jhu.jpg';
+export { default as m1 } from './m1.jpg';
+export { default as m2 } from './m2.jpg';
+export { default as m3 } from './m3.jpg';
+export { default as m4 } from './m4.jpg';
+export { default as m5 } from './m5.jpg';
+export { default as m6 } from './m6.jpg';
+export { default as m7 } from './m7.jpg';
+export { default as m8 } from './m8.jpg';
+
+export { default as cc } from './cc.jpg';
+export { default as p1 } from './p1.jpg';
+export { default as p2 } from './p2.jpg';
+export { default as p3 } from './p3.jpg';
--- my-app/src/components/Certs.jsx
+++ my-app/src/components/Certs.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { csx, css, csp, m1, m2, m3, m4, m5, m6, m7, m8 } from '../assets/certs';
+import { csx, css, csp, jhu, m1, m2, m3, m4, m5, m6, m7, m8, cc, p1, p2, p3 } from '../assets/certs';
 import Certificate from './Certificate';
 
 const Certs = () => {
   const [activeCategory, setActiveCategory] = useState('cs50');
 
+  // Certificate arrays
   const cs50Certs = [
     { image: csx, url: 'https://cs50.harvard.edu/certificates/9d966695-a4b6-4154-9952-52e4d2efa692', altText: 'CS50x Certificate' },
     { image: css, url: 'https://cs50.harvard.edu/certificates/a06a26e2-64e9-438a-8666-977af8d062ed', altText: 'CS50S Certificate' },
     { image: csp, url: 'https://cs50.harvard.edu/certificates/7226859c-c657-4563-b9c8-99f4873114f6', altText: 'CS50P Certificate' },
   ];
 
-  const otherCerts = [
+  const webCerts = [
+    { image: jhu, url: 'https://www.coursera.org/account/accomplishments/certificate/8DILH4N0ABM6', altText: 'JHU Web Dev Certificate' },
     { image: m1, url: 'https://www.coursera.org/account/accomplishments/certificate/MMU2WMZEKWOA', altText: 'Meta Front-End Intro Certificate' },
     { image: m2, url: 'https://www.coursera.org/account/accomplishments/certificate/LO2FMY48V1BX', altText: 'Meta JavaScript Certificate' },
     { image: m3, url: 'https://www.coursera.org/account/accomplishments/certificate/4NSF48QD3FIS', altText: 'Meta Version Control Certificate' },
@@ -22,9 +24,23 @@ const Certs = () => {
     { image: m8, url: 'https://www.coursera.org/account/accomplishments/certificate/MGPD0EVNC2FY', altText: 'Meta Capstone Certificate' },
   ];
 
+  const otherCerts = [
+    { image: cc, url: 'https://codecombat.com/certificates/6533825122ab2d0031d4ba79', altText: 'CodeCombat Certificate' },
+    { image: p1, url: 'https://www.coursera.org/account/accomplishments/certificate/NTMXA8WACBNQ', altText: 'UOM Python Intro Certificate' },
+    { image: p2, url: 'https://www.coursera.org/account/accomplishments/certificate/Z47RD2HY9PYR', altText: 'UOM Python Data Certificate' },
+    { image: p3, url: 'https://www.coursera.org/account/accomplishments/certificate/MTNK7QQL6XA3', altText: 'UOM Python Web Data Certificate' },
+  ];
+
+  // Render certificates based on active category
   const renderCertificates = () => {
-    const certificates =
-      activeCategory === 'cs50' ? cs50Certs : otherCerts;
+    let certificates = [];
+    if (activeCategory === 'cs50') {
+      certificates = cs50Certs;
+    } else if (activeCategory === 'web') {
+      certificates = webCerts;
+    } else if (activeCategory === 'other') {
+      certificates = otherCerts;
+    }
     return certificates.map((cert, index) => (
       <Certificate
         key={index}
@@ -39,6 +55,7 @@ const Certs = () => {
     <section className="p-80">
       <div className="container mx-auto">
         <h2 className="font-bold mb-6">Certificates</h2>
+        {/* Buttons to switch categories */}
         <div className="flex space-x-4 mb-6 justify-center">
           <button
             className={`px-4 py-2 rounded ${activeCategory === 'cs50' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
@@ -47,12 +64,19 @@ const Certs = () => {
             CS50
           </button>
           <button
-            className={`px-4 py-2 rounded ${activeCategory === 'coursera' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
-            onClick={() => setActiveCategory('coursera')}
+            className={`px-4 py-2 rounded ${activeCategory === 'web' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+            onClick={() => setActiveCategory('web')}
           >
-            Coursera
+            Web Dev & Meta
+          </button>
+          <button
+            className={`px-4 py-2 rounded ${activeCategory === 'other' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+            onClick={() => setActiveCategory('other')}
+          >
+            Other
           </button>
         </div>
+        {/* Certificate grid */}
         <div className="py-10 grid grid-cols-1 md:grid-cols-3 gap-10">
           {renderCertificates()}
         </div>
