9b32930caed9ca79349742a9869a87489746db68
VirakbothSoth <121625231+VirakbothSoth@users.noreply.github.com>
2025-01-11 18:10:34 +0700

    Added Django and Hobbies

--- my-app/src/components/Skills.jsx
+++ my-app/src/components/Skills.jsx
@@ -2,50 +2,48 @@ import React from 'react';
 
 const Skills = () => {
   const skills = [
-    { name: 'Scratch (Mastered!)', level: 100 },
-    { name: 'Python', level: 90 },
-    { name: 'C', level: 30 },
-    { name: 'HTML (Mastered!)', level: 100 },
+    { name: 'Python', level: 80 },
+    { name: 'C', level: 45 },
+    { name: 'HTML', level: 95 },
     { name: 'CSS', level: 90 },
-    { name: 'JavaScript', level: 75 },
-    { name: 'React (Learning)', level: 60 },
-    { name: 'Tailwind CSS (Learning)', level: 60 },
+    { name: 'JavaScript', level: 65 },
+    { name: 'React', level: 60 },
+    { name: 'Tailwind CSS', level: 60 },
+    { name: 'Django', level: 20 },
   ];
 
   // Function to get color based on skill name
   const getColor = (skillName) => {
     switch (skillName.toLowerCase()) {
-      case 'scratch (mastered!)':
-        return 'bg-orange-500';
-      case 'html (mastered!)':
+      case 'html':
         return 'bg-red-400';
       case 'python':
         return 'bg-blue-400';
       case 'javascript':
-        return 'bg-yellow-400';
+        return 'bg-green-400';
       case 'c':
         return 'bg-cyan-400';
       case 'css':
         return 'bg-blue-400';
-      case 'react (learning)':
+      case 'react':
         return 'bg-cyan-400';
-      case 'tailwind css (learning)':
+      case 'tailwind css':
         return 'bg-teal-400';
-      default:
-        return 'bg-gray-400';
+      case 'django':
+        return 'bg-orange-400';
     }
   };
 
   return (
-    <section className="py-80">
+    <section id="skills" className="py-20">
       <div className="container mx-auto">
-        <h2 className="font-bold mb-6">Skills</h2>
+        <h2 className="text-5xl text-center font-bold mb-6">Skills</h2>
         <div className="flex justify-center">
           <ul className="flex flex-wrap justify-center gap-8">
             {skills.map((skill, index) => (
               <li
                 key={index}
-                className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2 bg-blue-600 text-white rounded-md hover:shadow-xl hover:p-10 duration-500"
+                className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2 bg-blue-600 text-white rounded-md text-center hover:shadow-xl hover:p-5 duration-500"
               >
                 <h3 className="font-semibold text-3xl mb-2">{skill.name}</h3>
                 <div className="relative pt-1">
